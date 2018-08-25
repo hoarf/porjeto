bdb7c9c7df03be97994c9898819d3f691322ab40
Alan Ficagna <alan.ficagna@gmail.com>
2018-08-25 15:00:45 -0300

    Fix question query

--- lib/porjeto/evaluation.ex
+++ lib/porjeto/evaluation.ex
@@ -1,12 +1,21 @@
 defmodule Porjeto.Evaluation do
 
+  import Ecto.Query
   alias Ecto.Multi
-  alias Porjeto.Evaluation.{ User, Evaluation }
+  alias Porjeto.Evaluation.{Question, User }
+  alias App.Repo
 
-  def create_evaluation(user_email) do
+  def create_evaluation(params) do
     Multi.new
-    |> Multi.insert_or_update(:user, User.changeset(user_email))
-    |> Multi.insert(:evaluation, Evaluation.changeset(%{}))
+    |> Multi.insert_or_update(:user, User.changeset(User, params))
+    |> Multi.insert(:evaluation, %{user: User.changeset(User, params)})
+    |> Repo.transaction
   end
 
+  def questions(questionnaire_id) do
+    Question
+    |> join(:inner, [q], qi in assoc(q, :questionnaire))
+    |> where([q, qi], qi.id == ^questionnaire_id)
+    |> Repo.all
+  end
 end
