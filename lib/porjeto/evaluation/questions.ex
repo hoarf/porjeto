67f79d0075c81fb4afbe7053ccc4264853b53eab
Alan Ficagna <alan.ficagna@gmail.com>
2018-08-25 15:01:15 -0300

    Add many to many assoc to questions

--- lib/porjeto/evaluation/questions.ex
+++ lib/porjeto/evaluation/questions.ex
@@ -3,6 +3,7 @@ defmodule Porjeto.Evaluation.Question do
   use Ecto.Schema
 
   schema "questions" do
+    many_to_many :questionnaire, Porjeto.Evaluation.Questionnaire, join_through: "questions_list"
   end
 
 end
