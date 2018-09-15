664eb2b4bdf38be235b86d6e320945174ce8838c
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-15 16:23:09 -0300

    Add more fields to questions

--- lib/porjeto_web/views/question_view.ex
+++ lib/porjeto_web/views/question_view.ex
@@ -11,6 +11,13 @@ defmodule AppWeb.QuestionView do
   end
 
   def render("question.json", %{question: question}) do
-    %{description: question.description}
+    %{
+      description: question.description,
+      answer1: question.answer1,
+      answer2: question.answer2,
+      answer3: question.answer3,
+      answer4: question.answer4,
+      answer5: question.answer5,
+    }
   end
 end
