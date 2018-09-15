c6eea6e7323064d4726c78d90358a0ab5606c31d
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-15 16:23:30 -0300

    Add more question seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,12 +1,31 @@
 App.Repo.insert!(%Porjeto.Evaluation.Questionnaire{
       id: 1,
       name: "Test Questionnaire",
-      questions: [%Porjeto.Evaluation.Question{
-                     id: 1,
-                     description: "How many years have you been alive.",
-                     answer1: "10",
-                     answer2: "20",
-                     answer3: "30",
-                     answer4: "40",
-                     answer5: "50",
-                     }]} , on_conflict: :nothing)
+      questions: [
+        %Porjeto.Evaluation.Question{
+          id: 1,
+          description: "How many years have you been alive.",
+          answer1: "10",
+          answer2: "20",
+          answer3: "30",
+          answer4: "40",
+          answer5: "50",
+        },
+        %Porjeto.Evaluation.Question{
+          id: 2,
+          description: "How many years have you been alive.",
+          answer1: "10",
+          answer2: "20",
+          answer3: "30",
+          answer4: "40",
+          answer5: "50",
+        },
+        %Porjeto.Evaluation.Question{
+          id: 3,
+          description: "How many years have you been alive.",
+          answer1: "10",
+          answer2: "20",
+          answer3: "30",
+          answer4: "40",
+          answer5: "50",
+}]} , on_conflict: :nothing)
