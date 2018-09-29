9432617ff4f0c19159ca3858377d3d07e0cd808b
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-29 15:31:30 -0300

    No destructuring on controllers rule.

--- lib/porjeto_web/controllers/answer_controller.ex
+++ lib/porjeto_web/controllers/answer_controller.ex
@@ -5,7 +5,7 @@ defmodule AppWeb.AnswerController do
 
   action_fallback(AppWeb.DefaultFallbackController)
 
-  def create(conn, %{"evaluation_id" => evaluation_id, "question_id" => question_id, "value" => value} = params) do
+  def update(conn, params) do
     with {:ok, answer} <- Evaluation.create_or_update_answer(evaluation_id, question_id, value) |> Repo.insert_or_update do
       conn
       |> put_status(:ok)
