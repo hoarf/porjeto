abc3b2d39ba92cb81040da401a9acf3a2e9d94bb
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-29 15:09:37 -0300

    Add answer schema.

---
+++ lib/porjeto/evaluation/answer.ex
@@ -0,0 +1,17 @@
+defmodule Porjeto.Evaluation.Answer do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Porjeto.Evaluation.{ Question, Evaluation }
+
+  schema "answers" do
+    field(:value, :boolean)
+    belongs_to(:question, Question)
+    belongs_to(:evaluation, Evaluation)
+  end
+
+  def changeset(answer, params \\ %{}) do
+    answer
+    |> cast(params, [])
+    |> validate_required([:value, :evaluation_id, :question_id])
+  end
+end
